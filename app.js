6b25b5eb3f5bb28d2b4dfde814c672b12e1b6a98
dvthnight <davide.lanotte@protonmail.com>
2022-02-24 18:51:54 +0100

    viene mostrato primo elemento già attivo

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const slider = new Vue ({
                 item : "img/01.jpg",
                 title: "Svezia",
                 text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+                active: true,
             },
         
             {
@@ -21,19 +22,41 @@ const slider = new Vue ({
                 item : "img/03.jpg",
                 title:  "Gran Bretagna",
                 text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+                active: false,
             },
         
             {
                 item : "img/04.jpg",
                 title: "Germania",
                 text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+                active: false,
             },
         
             {
                 item : "img/05.jpg",
                 title: "Paradise",
                 text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+                active: false,
             },
         ]
+    },
+
+    methods:{
+
+        // attiva : function(){
+        //     for(let i =0; i<5; i++){
+        //         if(this.active==true){
+        //             return "active";
+        //         }
+        //     }
+        // },
+
+        anteprime : function (){
+            this.active = true; 
+            console.log(this.active);
+            attiva();
+        },
+
+       
     }
 })
